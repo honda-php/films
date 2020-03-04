a534e2bfbdf66fedde26c0497fd5e86d5f20ea77
Kana Honda <usausa.usami.0810@gmail.com>
2020-03-04 16:53:55 +0900

    fists-migrate

---
+++ database/migrations/2020_03_04_152437_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->string('use_camera')->nullable();
+            $table->string('use_film')->nullable();
+            $table->string('coment')->nullable();
+            $table->string('image_path');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
